89952e5ddb283a858846ca560b4dc60c3e962f7e
Jonathan Sch <jonathan.schaeberle@outlook.de>
2021-03-08 23:54:05 +0100

    Updaed Database installation instructions

--- datenbank/README.md
+++ datenbank/README.md
@@ -6,6 +6,12 @@ Die Datenbank basiert auf einer MySQL Datenbank.
 
 Am einfachsten ist die Datenbank mittels XAMPP installiert. Das ist ein Programm, um auf dem Rechner z.B. eine´Datenbank anzulegen.
 
-## Daten zurückspielen
+## Create Database for the first Time
+
+Create the Database in your favorite SQL Editor with `CREATE DATABASE IF NOT EXISTS 'web_chat';`
+
+The other Structure the backend will create automaticaly.
+
+## Daten zurückspielen statt eine leere Datenbank zu erstellen
 
 Um eine leere Datenbank zu erstellen kann die datei [web_chat-dump.sql](./web_chat-dump.sql) in der mysql Workbench (oder einem vergleichbaren Datenbank Verwaltungstool) ausgeführt werden, um eine leere Datenbank zu starten.
--- datenbank/web_chat-dump.sql
+++ datenbank/web_chat-dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `web_chat` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `web_chat`;
 -- MariaDB dump 10.17  Distrib 10.4.8-MariaDB, for Win64 (AMD64)
 --
 -- Host: 127.0.0.1    Database: web_chat
@@ -23,31 +25,54 @@ DROP TABLE IF EXISTS `chat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chat` (
-  `idchat` int(11) NOT NULL AUTO_INCREMENT,
-  `chatname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`idchat`),
-  UNIQUE KEY `idx_chatname` (`chatname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `chatname` varchar(45) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `matching_user_chat`
+-- Dumping data for table `chat`
 --
 
-DROP TABLE IF EXISTS `matching_user_chat`;
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+INSERT INTO `chat` VALUES (1,'TestChat1','2021-02-11 07:20:11','2021-02-11 07:20:11'),(2,'TestChat2','2021-02-11 07:24:04','2021-02-11 07:24:04'),(3,'TestChat3','2021-02-11 07:24:08','2021-02-11 07:24:08'),(4,'TestChat4','2021-02-11 07:24:11','2021-02-11 07:24:11'),(5,'TestChat5','2021-02-11 07:24:14','2021-02-11 07:24:14'),(6,'TestChat6','2021-02-11 07:24:17','2021-02-11 07:24:17');
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chatmembers`
+--
+
+DROP TABLE IF EXISTS `chatmembers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `matching_user_chat` (
-  `id_matching_user_chat` int(11) NOT NULL AUTO_INCREMENT,
-  `iduser` int(11) NOT NULL,
-  `idchat` int(11) NOT NULL,
-  PRIMARY KEY (`id_matching_user_chat`),
-  KEY `iduser` (`iduser`),
-  KEY `idchat` (`idchat`),
-  CONSTRAINT `matching_user_chat_ibfk_1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`),
-  CONSTRAINT `matching_user_chat_ibfk_2` FOREIGN KEY (`idchat`) REFERENCES `chat` (`idchat`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `chatmembers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `ChatId` int(10) unsigned DEFAULT NULL,
+  `UserId` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ChatMembers_UserId_ChatId_unique` (`ChatId`,`UserId`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `chatmembers_ibfk_131` FOREIGN KEY (`ChatId`) REFERENCES `chat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `chatmembers_ibfk_132` FOREIGN KEY (`UserId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chatmembers`
+--
+
+LOCK TABLES `chatmembers` WRITE;
+/*!40000 ALTER TABLE `chatmembers` DISABLE KEYS */;
+INSERT INTO `chatmembers` VALUES (1,'2021-02-11 07:20:11','2021-02-11 07:20:11',1,1),(2,'2021-02-11 07:24:04','2021-02-11 07:24:04',2,1),(3,'2021-02-11 07:24:08','2021-02-11 07:24:08',3,1),(4,'2021-02-11 07:24:11','2021-02-11 07:24:11',4,1),(5,'2021-02-11 07:24:14','2021-02-11 07:24:14',5,1),(6,'2021-02-11 07:24:17','2021-02-11 07:24:17',6,1),(7,'2021-02-11 07:50:21','2021-02-11 07:50:21',1,3),(8,'2021-02-11 07:52:59','2021-02-11 07:52:59',1,4),(11,'2021-02-11 07:54:07','2021-02-11 07:54:07',4,3);
+/*!40000 ALTER TABLE `chatmembers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `message`
@@ -57,18 +82,29 @@ DROP TABLE IF EXISTS `message`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `message` (
-  `idmessage` int(11) NOT NULL AUTO_INCREMENT,
-  `id_matching_user_chat` int(11) NOT NULL,
-  `iduser` int(11) NOT NULL,
-  `message` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `time` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`idmessage`),
-  KEY `id_matching_user_chat` (`id_matching_user_chat`),
-  KEY `iduser` (`iduser`),
-  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`id_matching_user_chat`) REFERENCES `matching_user_chat` (`id_matching_user_chat`),
-  CONSTRAINT `message_ibfk_2` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `message` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `UserId` int(10) unsigned DEFAULT NULL,
+  `ChatId` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `UserId` (`UserId`),
+  KEY `ChatId` (`ChatId`),
+  CONSTRAINT `message_ibfk_131` FOREIGN KEY (`UserId`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `message_ibfk_132` FOREIGN KEY (`ChatId`) REFERENCES `chat` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,'\"fdsafdsa\"','0000-00-00 00:00:00','0000-00-00 00:00:00',1,1);
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -78,13 +114,25 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`iduser`),
-  UNIQUE KEY `idx_username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'test123','Pw123','2021-02-05 10:00:00','2021-02-08 18:00:00'),(3,'jonny','beschte','2021-02-06 14:14:00','2021-02-08 16:31:00'),(4,'max','muster','2021-02-06 18:35:00','2021-02-08 17:54:00'),(5,'TestUser_Updated','string','2021-02-09 13:50:42','2021-02-09 13:53:25');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -95,4 +143,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-12  9:01:39
+-- Dump completed on 2021-03-08 23:51:51
